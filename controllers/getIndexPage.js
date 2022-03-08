c3dd7c44c28dde2a239eba67314ab6b8ef71acbf
Sakshi Garg <sakshigarg0180@gmail.com>
2022-03-08 19:48:17 +0530

    one commit merged

--- controllers/getIndexPage.js
+++ controllers/getIndexPage.js
@@ -2,6 +2,16 @@ const getIndexPage = (req, res) => {
     res.render('index');
 }
 
+const getEventPage = (req, res) => {
+    res.render('eventPage');
+}
+
+const getLaunchPage = (req , res) => {
+    res.render('launchPage');
+}
+
 module.exports = {
     getIndexPage,
+    getEventPage,
+    getLaunchPage,
 }
